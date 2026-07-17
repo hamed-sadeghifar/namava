dc30ef061f7f1c72a5200150603ba4fbc3526cb4
hamed <hamedsadeghifar@gmail.com>
2026-07-17 17:39:33 +0330

    responsive section one

--- src/assets/section 1/index.js
+++ src/assets/section 1/index.js
@@ -8,4 +8,35 @@ import pic7 from "./pic7.jpg";
 import pic8 from "./pic8.jpg";
 import pic9 from "./pic9.jpg";
 import pic10 from "./pic10.jpg";
-export { pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10 };
+import pic11 from "./768/pic1.jpg";
+import pic12 from "./768/pic2.jpg";
+import pic13 from "./768/pic3.jpg";
+import pic14 from "./768/pic4.jpg";
+import pic15 from "./768/pic5.jpg";
+import pic16 from "./768/pic6.jpg";
+import pic17 from "./768/pic7.jpg";
+import pic18 from "./768/pic8.jpg";
+import pic19 from "./768/pic9.jpg";
+import pic20 from "./768/pic10.jpg";
+export {
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+  pic6,
+  pic7,
+  pic8,
+  pic9,
+  pic10,
+  pic11,
+  pic12,
+  pic13,
+  pic14,
+  pic15,
+  pic16,
+  pic17,
+  pic18,
+  pic19,
+  pic20,
+};
--- src/components/content/SectionOne.jsx
+++ src/components/content/SectionOne.jsx
@@ -1,7 +1,7 @@
+import { useEffect, useRef, useState } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import {
   pic1,
-  pic10,
   pic2,
   pic3,
   pic4,
@@ -10,13 +10,72 @@ import {
   pic7,
   pic8,
   pic9,
+  pic10,
+  pic11,
+  pic12,
+  pic13,
+  pic14,
+  pic15,
+  pic16,
+  pic17,
+  pic18,
+  pic19,
+  pic20,
 } from "../../assets/section 1/index";
-import { useState } from "react";
 const SectionOne = () => {
   const [page, setPage] = useState(0);
   const [flag, setFlag] = useState(false);
-  const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10];
-  const totalPages = Math.ceil(images.length / 3);
+  const [cardWidth, setCardWidth] = useState(0);
+  const [visibleCards, setVisibleCards] = useState(3);
+
+  const slideRef = useRef(null);
+
+  const bigImages = [
+    pic1,
+    pic2,
+    pic3,
+    pic4,
+    pic5,
+    pic6,
+    pic7,
+    pic8,
+    pic9,
+    pic10,
+  ];
+  const smallImages = [
+    pic11,
+    pic12,
+    pic13,
+    pic14,
+    pic15,
+    pic16,
+    pic17,
+    pic18,
+    pic19,
+    pic20,
+  ];
+  const images = window.innerWidth >= 768 ? bigImages : smallImages;
+  const totalPages = Math.ceil(bigImages.length / visibleCards);
+
+  useEffect(() => {
+    const updateCardSize = () => {
+      if (!slideRef.current) return;
+      const firstCart = slideRef.current.children[0];
+      if (!firstCart) return;
+      if (window.innerWidth >= 1280) {
+        setVisibleCards(3);
+      } else {
+        setVisibleCards(2);
+      }
+      setCardWidth(firstCart.offsetWidth);
+    };
+    updateCardSize();
+    window.addEventListener("resize", updateCardSize);
+    return () => {
+      window.removeEventListener("resize", updateCardSize);
+    };
+  }, []);
+
   return (
     <section
       className="my-10 flex relative overflow-hidden"
@@ -24,7 +83,7 @@ const SectionOne = () => {
       onMouseLeave={() => setFlag(false)}
     >
       <button
-        className="transition-opacity cursor-pointer absolute text-white left-0 bottom-0 opacity-0 z-10 py-20 px-3"
+        className={`transition-opacity cursor-pointer absolute text-white left-0 bottom-0 opacity-0 z-10 h-full px-3`}
         style={flag && page < totalPages - 1 ? { opacity: 1 } : { opacity: 0 }}
         onClick={() =>
           setPage((prev) => (page < totalPages - 1 ? prev + 1 : prev))
@@ -33,69 +92,29 @@ const SectionOne = () => {
         <FaAngleLeft className="text-2xl" />
       </button>
       <button
-        className="transition-opacity cursor-pointer absolute text-white right-0 bottom-0 opacity-0 z-10 py-20 px-3"
+        className="transition-opacity cursor-pointer absolute text-white right-0 bottom-0 opacity-0 z-10 h-full px-3"
         style={flag && page !== 0 ? { opacity: 1 } : { opacity: 0 }}
         onClick={() => setPage((prev) => (page > 0 ? prev - 1 : prev))}
       >
         <FaAngleRight className="text-2xl" />
       </button>
       <div
-        className="flex mx-10"
+        ref={slideRef}
+        className="flex xl:mx-10 md:mx-8 mx-6"
         style={{
-          transform: `translateX(${page * 1450}px)`,
+          transform: `translateX(${page * (cardWidth + 16) * 2}px)`,
           transition: "transform 1s ease-in 0.1s",
         }}
       >
-        <img
-          src={pic1}
-          alt="pic1"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic2}
-          alt="pic2"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic3}
-          alt="pic3"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic4}
-          alt="pic4"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic5}
-          alt="pic5"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic6}
-          alt="pic6"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic7}
-          alt="pic7"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic8}
-          alt="pic8"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic9}
-          alt="pic9"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
-        <img
-          src={pic10}
-          alt="pic10"
-          className="ml-4 w-117 h-44 rounded-xl cursor-pointer shrink-0"
-        />
+        {images.map((image, index) => (
+          <div
+            className="ml-4 xl:max-w-[calc((100%-2rem)/3)] max-w-[calc((100%-1rem)/2)]
+            shrink-0 rounded-xl overflow-hidden cursor-pointer"
+            key={index}
+          >
+            <img src={image} alt="pic1" />
+          </div>
+        ))}
       </div>
     </section>
   );
