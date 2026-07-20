dcc3e5575efface3eb3cc7abf1603115b75a9b11
hamed <hamedsadeghifar@gmail.com>
2026-07-20 13:18:11 +0330

    edit high light slider section

--- src/components/content/HighLight.jsx
+++ src/components/content/HighLight.jsx
@@ -31,7 +31,7 @@ const HighLight = () => {
       }
       setCardWidth(firstCart.getBoundingClientRect().width);
     };
-    updateCardSize();
+    window.addEventListener("load", updateCardSize);
     window.addEventListener("resize", updateCardSize);
     return () => {
       window.removeEventListener("resize", updateCardSize);
